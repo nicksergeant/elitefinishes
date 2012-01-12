bd51cfc7fa093f8aee08b5d5fd576ad593ea0771
Nick Sergeant <nick@nicksergeant.com>
2012-01-11 22:25:31 -0500

    up

---
+++ public/includes/js_scripts.js
@@ -0,0 +1,8 @@
+<!--
+home_btn_1_hover = new Image();
+home_btn_1_hover.src = "http://elitefinishes.com/images/home_btn_1_hover.jpg";
+home_btn_2_hover = new Image();
+home_btn_2_hover.src = "http://elitefinishes.com/images/home_btn_2_hover.jpg";
+home_btn_3_hover = new Image();
+home_btn_3_hover.src = "http://elitefinishes.com/images/home_btn_3_hover.jpg";
+//-->
